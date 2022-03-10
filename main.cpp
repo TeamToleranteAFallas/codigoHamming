7b97a1e1eb851fc4cf3c4313ee8e6359b996112b
ReneGarBer <67211759+ReneGarBer@users.noreply.github.com>
2022-03-10 02:11:45 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Detección y corrección de errores
+Código Hamming
+Computación tolerante a fallas
+Equipo 01
+--
+--
+--
+--
+Garcia Bernal Rene
+09/03/2022
+*/
+
+#include "Menu.h"
+
+int main(){
+    Menu menu;
+    return 0;
+}
